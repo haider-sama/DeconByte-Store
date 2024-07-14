0db2f9c0805607eb4ac4c15aa8c23274df4b99ac
Haider <47008588+haider-sama@users.noreply.github.com>
2024-07-14 17:24:30 +0500

    Add files via upload

---
+++ env.py
@@ -0,0 +1,8 @@
+DATABASE_URL = "mongodb+srv://vegetakhan2000:vegetakhan2000@cluster0.nlreyea.mongodb.net/ecommerce-store-deconbyte-01"
+STRIPE_SECRET_KEY = "sk_test_51PUb1bFXcPIkJKVBjTHHsx6xK4UV0IjLRTeMb2jmgu5m3acpIJy7F349FOsq4V2B4nQYEqVpEvBNgdVClgDK2cos001r8BUrTJ"
+STRIPE_PUBLISHABLE_KEY = "pk_test_51PUb1bFXcPIkJKVBZ7bnLLnS0em06KL3KV9Ei9xXqKluHNH34bEzQZhigdnDNDBXn4V9s6OrKGiBtO87BElk8T3O00ggxm49vT"
+GOOGLE_CLIENT_ID = "85645214746-6af2tr93ndrci7dks9isupfp309ajl9k.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+NEXTAUTH_SECRET = "ecommere-store-deconbyte-01-jwt"
+GITHUB_CLIENT_ID = ""
+GITHUB_CLIENT_SECRET = ""
